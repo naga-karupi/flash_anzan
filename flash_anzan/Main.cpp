94b11ef25bfccca5dc663abe6428495054b0e366
naga <toritonsuibu0527@gmail.com>
2023-08-07 12:43:34 +0900

    カスタム画面からタイトル画面への遷移の追加 コメントの追加 ゲーム表示のコンストラクタを書いた

--- flash_anzan/Main.cpp
+++ flash_anzan/Main.cpp
@@ -6,8 +6,11 @@ struct GameInfo{
 	SecondsF all_seconds;
 };
 
+
+
 using App = SceneManager<String, GameInfo>;
 
+/// @brief タイトル画面を表示するクラス
 class Title : public App::Scene {
 	Font title_font{ FontMethod::MSDF, 48, Typeface::Bold };
 
@@ -46,6 +49,7 @@ public:
 	}
 };
 
+/// @brief 任意の数値を背ってするための画面
 class Custom: public App::Scene {
 public:
 	TextEditState all_seconds;
@@ -67,18 +71,31 @@ public:
 	}
 
 	void update() override {
-		SimpleGUI::TextBox(all_seconds, Vec2{ 100,100 });
-		SimpleGUI::TextBox(digit_num, Vec2{ 100,150 });
-		SimpleGUI::TextBox(phase_num, Vec2{ 100,200 });
+		SimpleGUI::TextBox(all_seconds, Vec2{ 100, 100 });
+		SimpleGUI::TextBox(digit_num, Vec2{ 100, 150 });
+		SimpleGUI::TextBox(phase_num, Vec2{ 100, 200 });
+
+		if (SimpleGUI::Button(U"Back", Vec2{ 20, 20 })) {
+			changeScene(U"Title");
+		}
 
 		if (SimpleGUI::Button(U"Start", Vec2{ 300, 200 })) {
 			if (!all_seconds.text.isEmpty()
 				&& !digit_num.text.isEmpty()
 				&& !phase_num.text.isEmpty()) {
 
-				getData().all_seconds = SecondsF(Parse<double>(all_seconds.text));
-				getData().digit_num = Parse<int>(digit_num.text);
-				getData().phase_num = Parse<int>(phase_num.text);
+				double all_seconds_d = Parse<double>(all_seconds.text);
+				int digit_num_i = Parse<int>(digit_num.text);
+				int phase_num_i = Parse<int>(phase_num.text);
+
+				if (all_seconds_d < 0.0
+					|| digit_num_i < 0
+					|| phase_num_i < 0)
+					return;
+
+				getData().all_seconds = SecondsF(all_seconds_d);
+				getData().digit_num = digit_num_i;
+				getData().phase_num = phase_num_i;
 
 				changeScene(U"GameWaitingScene", 0s);
 			}
@@ -88,6 +105,7 @@ public:
 
 };
 
+/// @brief ゲーム開始前の一時待機画面
 class GameWaitingScene : public App::Scene {
 	Font description_font{ FontMethod::MSDF, 40, Typeface::Bold };
 
@@ -112,6 +130,7 @@ public:
 	}
 };
 
+/// @brief カウントダウン・スタート表示
 class CountDownScene : public App::Scene {
 	Stopwatch stopwatch;
 	Font sec_font{ FontMethod::MSDF, 40, Typeface::Bold };
@@ -141,10 +160,52 @@ public:
 
 };
 
+/// @brief 実際のゲームの表示
 class PlayGameScene : public App::Scene {
 
-public:
+	int answer;
+	Array<int> phase_nums;
+	Stopwatch stopwatch;
+	int now_phase;
 
+	SecondsF one_pannel_sec;
+
+	Font show_font{ FontMethod::MSDF, 40, Typeface::Bold };
+
+
+public:
+	PlayGameScene(const InitData& init)
+		: IScene(init), answer(0), phase_nums(getData().phase_num), now_phase(0) {
+
+		int max_num = 1;
+		int mini_num = 1;
+
+		for (int i = 0; i < getData().digit_num; i++) {
+			max_num *= 10;
+			mini_num *= 10;
+		}
+
+		max_num--;
+		mini_num /= 10;
+
+		for (auto& num : phase_nums) {
+			answer += num = Random(mini_num, max_num);
+		}
+
+		stopwatch.start();
+	}
+
+	~PlayGameScene() {
+		//pass
+	}
+
+	void draw() const override {
+		
+	}
+
+	void update() override {
+
+	}
 };
 
 
